4af2ef0eb3c80e3256ebaead0f82dc9c49df180d
chris <chba6499@colorado.edu>
2018-08-29 14:24:33 -0600

    Fixed folder structure

--- main.go
+++ main.go
@@ -1,17 +1,17 @@
 package main
 
 import (
-	"fmt"
 	"os"
+
 	service "github.com/christopherbate/go-service/service"
 )
 
 func main() {
 	port := os.Getenv("PORT")
-	if len(port) == 0 (
+	if len(port) == 0 {
 		port = "3000"
-	)
+	}
 
 	server := service.NewServer()
-	server.Run(":"+port)
+	server.Run(":" + port)
 }
